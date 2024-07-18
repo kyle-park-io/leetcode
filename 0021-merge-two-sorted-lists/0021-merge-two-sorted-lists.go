f8138f791801fe0e499d4a66dbdaf51dcd0e401d
kyle <88130007+kyle-park-io@users.noreply.github.com>
2024-07-18 20:23:55 +0900

    Time: 3 ms (61.39%), Space: 2.9 MB (18.19%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.go
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.go
@@ -79,3 +79,28 @@ func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 		return node
 	}
 }
+
+// func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+
+// 	var result []int
+
+// 	for list1 != nil {
+// 		result = append(result, list1.Val)
+// 		list1 = list1.Next
+// 	}
+// 	for list2 != nil {
+// 		result = append(result, list2.Val)
+// 		list2 = list2.Next
+// 	}
+// 	sort.Ints(result)
+
+// 	dummy := &ListNode{}
+// 	current := dummy
+// 	for _, val := range result {
+// 		newNode := &ListNode{Val: val}
+// 		current.Next = newNode
+// 		current = newNode
+// 	}
+
+// 	return dummy.Next
+// }
