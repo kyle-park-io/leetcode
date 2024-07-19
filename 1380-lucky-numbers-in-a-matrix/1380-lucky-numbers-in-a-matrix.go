ef6ae108e7173320760178aafe419ad6328795da
kyle <88130007+kyle-park-io@users.noreply.github.com>
2024-07-19 18:56:06 +0900

    Time: 12 ms (72.22%), Space: 5.4 MB (100%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.go
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.go
@@ -3,6 +3,7 @@ func luckyNumbers(matrix [][]int) []int {
 	check := 0
 	max := int(math.Pow(10, 5)) + 1
 	x := -1
+	check_x := make([]bool, len(matrix))
 	result := make([]int, 0)
 
 	for i := 0; i < len(matrix[0]); i++ {
@@ -16,12 +17,12 @@ func luckyNumbers(matrix [][]int) []int {
 
 		for j := 0; j < len(matrix[0]); j++ {
 			if max > matrix[x][j] {
-				// y = j
 				max = matrix[x][j]
 			}
 		}
 
-		if check == max {
+		if check == max && !check_x[x] {
+			check_x[x] = true
 			result = append(result, check)
 		}
 
@@ -33,3 +34,39 @@ func luckyNumbers(matrix [][]int) []int {
 
 	return result
 }
+
+// func luckyNumbers(matrix [][]int) []int {
+
+// 	check := 0
+// 	max := int(math.Pow(10, 5)) + 1
+// 	x := -1
+// 	result := make([]int, 0)
+
+// 	for i := 0; i < len(matrix[0]); i++ {
+
+// 		for j := 0; j < len(matrix); j++ {
+// 			if check < matrix[j][i] {
+// 				x = j
+// 				check = matrix[j][i]
+// 			}
+// 		}
+
+// 		for j := 0; j < len(matrix[0]); j++ {
+// 			if max > matrix[x][j] {
+// 				// y = j
+// 				max = matrix[x][j]
+// 			}
+// 		}
+
+// 		if check == max {
+// 			result = append(result, check)
+// 		}
+
+// 		check = 0
+// 		max = int(math.Pow(10, 5)) + 1
+// 		x = -1
+
+// 	}
+
+// 	return result
+// }
