d73b1224d1ced5c6b947d0091b7261511dfae49d
kyle <88130007+kyle-park-io@users.noreply.github.com>
2024-07-20 19:22:16 +0900

    Time: 70 ms (25.48%), Space: 7.2 MB (37.52%) - LeetHub

--- 0322-coin-change/0322-coin-change.go
+++ 0322-coin-change/0322-coin-change.go
@@ -1,18 +1,68 @@
 func coinChange(coins []int, amount int) int {
-    dp := make([]int, amount+1)
+
+	dp := make([]int, amount+1)
 
 	for i := 1; i < len(dp); i++ {
 		dp[i] = amount + 1
 	}
-	for i := 1; i <= amount; i++ {
-		for _, coin := range coins {
-			if i-coin >= 0 {
-				dp[i] = min(dp[i], 1+dp[i-coin])
+
+	for i := 0; i < len(dp); i++ {
+		for _, val := range coins {
+			if i+val > amount {
+				continue
+			}
+
+			if dp[i+val] > dp[i]+1 {
+				dp[i+val] = dp[i] + 1
 			}
 		}
 	}
-	if dp[amount] != amount+1 {
-		return dp[amount]
+	fmt.Println(dp)
+
+	if dp[len(dp)-1] == amount+1 {
+		return -1
 	}
-	return -1
+	return dp[len(dp)-1]
 }
+
+// // wrong answer
+// func coinChange(coins []int, amount int) int {
+
+// 	result := -1
+// 	current := 0
+
+// 	dp := make([]int, amount+1)
+// 	sort.Sort(sort.Reverse(sort.IntSlice(coins)))
+
+// 	if amount == 0 {
+// 		return 0
+// 	}
+
+// 	for _, val := range coins {
+// 		for {
+// 			if current+val > amount {
+// 				break
+// 			}
+// 			if dp[current+val] > dp[current]+1 || dp[current+val] == 0 {
+// 				dp[current+val] = dp[current] + 1
+// 			}
+// 			current += val
+// 		}
+// 		current = 0
+// 	}
+// 	fmt.Println(dp)
+
+// 	for i := 0; i < len(dp); i++ {
+// 		if result == -1 {
+// 			result = (dp[i] + dp[amount-i])
+// 		} else if dp[i] != 0 {
+// 			result = int(math.Min(float64(result), float64(dp[i]+dp[amount-i])))
+// 		}
+// 	}
+
+// 	if dp[amount] == 0 {
+// 		return -1
+// 	}
+
+// 	return result
+// }
