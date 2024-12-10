00edd3f827224228fac7fc3689fb39190a455218
kyle <88130007+kyle-park-io@users.noreply.github.com>
2024-12-10 15:48:30 +0900

    Time: 0 ms (100%), Space: 4.3 MB (12.19%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.go
@@ -0,0 +1,48 @@
+func merge(nums1 []int, m int, nums2 []int, n int) {
+
+	result := make([]int, m+n)
+	if n == 0 {
+		result = nums1[:m]
+		// nums1 = result
+		copy(nums1, result)
+		return
+	}
+
+	trial := 0
+	trial_x := 0
+	trial_y := 0
+	for {
+		if trial == m+n {
+			// nums1 = result
+			copy(nums1, result)
+			fmt.Println(nums1)
+			return
+		}
+
+		if trial_y >= n {
+			if trial_x < m {
+				result[trial] = nums1[trial_x]
+				trial_x++
+			}
+
+			trial++
+			continue
+		}
+
+		if trial_x >= m {
+			result[trial] = nums2[trial_y]
+			trial_y++
+		} else {
+			if nums1[trial_x] < nums2[trial_y] {
+				result[trial] = nums1[trial_x]
+				trial_x++
+			} else {
+				result[trial] = nums2[trial_y]
+				trial_y++
+			}
+		}
+
+		trial++
+	}
+
+}
