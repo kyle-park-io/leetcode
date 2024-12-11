7d17d9afe395ab32f2665c7fd6db874dbd541e3e
kyle <88130007+kyle-park-io@users.noreply.github.com>
2024-12-12 01:28:06 +0900

    Time: 0 ms (100%), Space: 9.3 MB (6.16%) - LeetHub

--- 0169-majority-element/0169-majority-element.go
+++ 0169-majority-element/0169-majority-element.go
@@ -1,16 +1,33 @@
 func majorityElement(nums []int) int {
-	ans := 0
-	count := 0
-	arr := make(map[int]int)
+    var ans int
+    var count int
 
-	for _, num := range nums {
-		arr[num]++
-		if arr[num] > count {
-			count = arr[num]
-			ans = num
-		}
-	}
+    for _, num := range nums {
+        if count == 0 {
+            ans = num
+        }
+        if num == ans {
+            count++
+        } else {
+            count--
+        }
+    }
 
-	fmt.Println(ans)
-	return ans
+    return ans
+
+    // use map
+	// ans := 0
+	// count := 0
+	// arr := make(map[int]int)
+
+	// for _, num := range nums {
+	// 	arr[num]++
+	// 	if arr[num] > count {
+	// 		count = arr[num]
+	// 		ans = num
+	// 	}
+	// }
+
+	// fmt.Println(ans)
+	// return ans
 }
