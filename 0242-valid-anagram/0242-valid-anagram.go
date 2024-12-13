3665f3b7f4c44f5a64a82314faa10fdc7352b24e
kyle <88130007+kyle-park-io@users.noreply.github.com>
2024-12-13 17:10:47 +0900

    Time: 0 ms (100%), Space: 4.8 MB (25.56%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.go
+++ 0242-valid-anagram/0242-valid-anagram.go
@@ -1,10 +1,21 @@
 func isAnagram(s string, t string) bool {
 	// UTF-8(ASCII)
 	// a~z (97~122)
+
+	// s
 	s_arr1 := [26]int{}
+	s_arr2 := make([]int, 26)
+	s_arr3 := make(map[int]int)
+	_ = s_arr1
+	_ = s_arr2
+	_ = s_arr3
+	// t
 	t_arr1 := [26]int{}
-	// arr2 := make([]int, 26)
-	// arr3 := make(map[int]struct{})
+	t_arr2 := make([]int, 26)
+	t_arr3 := make(map[int]int)
+	_ = t_arr1
+	_ = t_arr2
+	_ = t_arr3
 
 	for _, v := range s {
 		s_arr1[v-97]++
